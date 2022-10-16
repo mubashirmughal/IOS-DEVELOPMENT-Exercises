e62c0d29d70ddebdd5f105064f5be310c09e25a2
Muhammad Mubashir Mughal <donleo26@gmail.com>
2022-10-16 12:10:47 -0700

    first commit

--- helloworld/ViewController.swift
+++ helloworld/ViewController.swift
@@ -9,11 +9,27 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var counter = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
 
+    @IBAction func button1(_ sender: UIButton) {
+        
+        if counter == 0 {
+            view.backgroundColor = UIColor.blue
+        } else if counter == 1 {
+            view.backgroundColor = UIColor.red
+        }
+        else if counter == 2 {
+            view.backgroundColor = UIColor.green
+            counter = 0
+            return
+        }
+        counter += 1
+    }
 }
 
